32b495c1d8d1a244327d00325e1e2f3a79897ebd
KevinTa <loneshark26@yahoo.com>
2021-04-02 13:05:56 -0700

    Signup/Login Firebase
    Signup and Login feature using Firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,119 @@
 
-import React, { Component } from "react";
-import Root from "./Root"; 
-import { Route, Switch } from "react-router-dom"; 
-import Home from "./components/Home";
-import Signup from "./components/signup/Signup";
-import Login from "./components/login/Login";
-import Dashboard from "./components/dashboard/Dashboard";
+import React, {useState, useEffect} from "react";
+import fire from "./fire"
+import "./App.css";
+import Login from "./Login";
+import Hero from "./Hero";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Root> 
-          <Switch>
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route exact path="/" component={Home} />
-            <Route path="*">Ups</Route>
-          </Switch>
-        </Root> 
+const App = () => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  };
+
+  const clearErrors =() => {
+    setEmailError('');
+    setPasswordError('');
+  };
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email,password)
+      .catch(err =>{
+        switch(err.code){
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+          }
+
+        });
+
+    };
+    const handleSignup = () => {
+      clearErrors();
+      if(password == confirmPassword)
+      {
+      fire
+        .auth()
+        .createUserWithEmailAndPassword(email,password)
+        .catch(err =>{
+          switch(err.code){
+            case "auth/email-already-in-use":
+            case "auth/invalid-email":
+              setEmailError(err.message);
+              break;
+            case "auth/weak-password":
+              setPasswordError(err.message);
+            }
+  
+          });
+        }
+        else
+        {
+          setPasswordError("Passwords do not match");
+        }
+  
+      };
+    const handleLogout = () => {
+      fire.auth().signOut();
+    };
+
+    const authListener = () => {
+      fire.auth().onAuthStateChanged((user) => {
+        if(user)
+        {
+          clearInputs();
+          setUser(user);
+        }
+
+        else
+        {
+          setUser('');
+        }
+      
+      });
+    };
+
+    useEffect(() => {
+      authListener();
+    }, []);
+
+  return(
+    <div className ="App">
+      {user ? (
+        <Hero handleLogout = {handleLogout} />
+      ) : (
+        <Login
+        email = {email}
+        setEmail = {setEmail}
+        password = {password}
+        setPassword = {setPassword}
+        confirmPassword = {confirmPassword}
+        setConfirmPassword = {setConfirmPassword}
+        handleLogin = {handleLogin}
+        handleSignup = {handleSignup}
+        hasAccount = {hasAccount}
+        setHasAccount = {setHasAccount}
+        emailError = {emailError}
+        passwordError={passwordError}
+      />
+      )}
       </div>
-    );
-  }
-}
+  );
+};
 
 export default App;
